16877e1d51aaf0b2f5a48cab305d757e8eac5f0c
saveliy <bazhenovsp2005@mail.ru>
2023-02-07 12:25:56 +0500

    fix codec

---
+++ config.py
@@ -0,0 +1,8 @@
+import os
+
+file_name = "123.pdf"
+menu_file_name = "food_today.txt"
+file_temp = "123.txt"
+file_all_food_list = "result.txt"
+TOKEN = os.getenv("TOKEN")
+admin_id = os.getenv("ADMIN_ID")
